9be2641603c4eb2f815c114d7d48efb66cd63451
neerajnerlekar <neeraj.nerlekar@gmail.com>
2023-05-15 22:33:42 -0600

    finish 01-deploy-myerc20votes script with eoa signer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,38 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
 import "./tasks/accounts"
+import * as dotenv from "dotenv"
+dotenv.config()
+
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL ||
+  "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "localhost",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.18",
   paths: { tests: "tests" },
 }
